b6d4d93373594b80f1afbc287da8ff292befab78
James Hooper <jameshooperdesigns@gmail.com>
2019-03-19 18:17:10 -0600

    Edit the grid

---
+++ script.js
@@ -0,0 +1,56 @@
+
+
+TweenMax.from("#a", 3, {opacity:0, scale:0.5});
+TweenMax.from("#b", 3, {opacity:0, scale:0.5});
+TweenMax.from("#c", 3, {opacity:0, scale:0.5});
+TweenMax.from("#d", 3, {opacity:0, scale:0.5});
+TweenMax.from("#e", 3, {opacity:0, scale:0.5});
+TweenMax.from("#f", 3, {opacity:0, scale:0.5});
+TweenMax.from("#g", 3, {opacity:0, scale:0.5});
+TweenMax.from("#h", 3, {opacity:0, scale:0.5});
+TweenMax.from("#i", 3, {opacity:0, scale:0.5});
+TweenMax.from("#j", 3, {opacity:0, scale:0.5});
+
+TweenMax.from("#k", 3, {opacity:0, scale:0.5});
+TweenMax.from("#l", 3, {opacity:0, scale:0.5});
+TweenMax.from("#m", 3, {opacity:0, scale:0.5});
+TweenMax.from("#n", 3, {opacity:0, scale:0.5});
+TweenMax.from("#o", 3, {opacity:0, scale:0.5});
+TweenMax.from("#p", 3, {opacity:0, scale:0.5});
+TweenMax.from("#q", 3, {opacity:0, scale:0.5});
+TweenMax.from("#r", 3, {opacity:0, scale:0.5});
+TweenMax.from("#s", 3, {opacity:0, scale:0.5});
+TweenMax.from("#t", 3, {opacity:0, scale:0.5});
+
+TweenMax.from("#u", 3, {opacity:0, scale:0.5});
+TweenMax.from("#v", 3, {opacity:0, scale:0.5});
+TweenMax.from("#w", 3, {opacity:0, scale:0.5});
+TweenMax.from("#x", 3, {opacity:0, scale:0.5});
+TweenMax.from("#y", 3, {opacity:0, scale:0.5});
+TweenMax.from("#z", 3, {opacity:0, scale:0.5});
+TweenMax.from("#a1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#b1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#c1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#d1", 3, {opacity:0, scale:0.5});
+
+TweenMax.from("#e1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#f1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#g1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#h1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#i1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#j1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#k1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#l1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#m1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#n1", 3, {opacity:0, scale:0.5});
+
+TweenMax.from("#o1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#p1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#q1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#r1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#s1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#t1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#u1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#v1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#w1", 3, {opacity:0, scale:0.5});
+TweenMax.from("#x1", 3, {opacity:0, scale:0.5});
