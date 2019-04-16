214c0864ea747a6c0ed91bb37ad40dc718fe9fc7
James Hooper <jameshooperdesigns@gmail.com>
2019-04-15 23:21:18 -0600

    Edit the script

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-/* Logo rotation Icon */
-TweenLite.to(".logo", 2, { ease: "hop", scale:1, rotation:1080 });
-
-
 TweenMax.staggerFrom(".btn", 2, {scale:0.5, opacity:0, delay:0.5, ease:Elastic.easeOut, force3D:true}, 0.2);
 $(".btn").click(function(){
 TweenMax.staggerTo(".btn", 0.5, {opacity:0, y:-100, ease:Back.easeIn}, 0.1);
 });
+
+
+/* Logo rotation Icon */
+TweenLite.to(".logo", 2, { ease: "hop", scale:1, rotation:1080 });
+
+
+
 
