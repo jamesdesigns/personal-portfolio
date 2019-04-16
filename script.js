d48dfda2dca62a298b12168aabc18720221cc980
James Hooper <jameshooperdesigns@gmail.com>
2019-04-15 23:12:27 -0600

    Add TweenMax to script

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 TweenLite.to(".logo", 2, { ease: "hop", scale:1, rotation:1080 });
 
 
+TweenMax.staggerFrom(".btn", 2, {scale:0.5, opacity:0, delay:0.5, ease:Elastic.easeOut, force3D:true}, 0.2);
+$(".btn").click(function(){
+TweenMax.staggerTo(".btn", 0.5, {opacity:0, y:-100, ease:Back.easeIn}, 0.1);
+});
+
