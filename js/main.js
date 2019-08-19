bc69e2bd88241876cf9882e3b0f8c5aec34d19da
James Hooper <jameshooperdesigns@gmail.com>
2019-08-19 16:10:06 -0600

    Add many changes to the website including, GreenSock animations and content updates

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	"use strict";
+	let container = $('#name');
+    TweenLite.from(container, 1, {marginTop: 800, backgroundColor: "black"});
+});
+
+$(document).ready(function() {
+	"use strict";
+	let container = $('#profile');
+    TweenLite.from(container, 1, {marginLeft: 500, backgroundColor: "black"});
+});
+
+$(document).ready(function() {
+	"use strict";
+	let container = $('.portfolio');
+    TweenLite.from(container, 1, {marginTop: 800, backgroundColor: "black"});
+});
+
+$(document).ready(function() {
+	"use strict";
+	let container = $('#regForm');
+    TweenLite.from(container, 1, {marginLeft: 800, backgroundColor: "black"});
+});
+
+
