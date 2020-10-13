bb7ab639c1abfd7b8b71f60fefdcfaaf53faa549
James Hooper <jameshooperdesigns@gmail.com>
2020-10-13 16:34:17 -0600

    Add new changes to this project

--- contact.php
+++ contact.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>James Hooper Designs- Contact Me</title>
+<title>Artist James Hooper - Contact Me</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://fonts.googleapis.com/css?family=Oswald:600" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://use.typekit.net/shu4idq.css">
-<link href="mystylesversion36.css" rel="stylesheet" type="text/css">
+<link href="mystylesVersion28.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <div id="siteWrapper">
